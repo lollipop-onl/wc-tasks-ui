1c4063d801930b7e4fce2c75946120908a7f33f8
lollipop-onl <lollipop@simochee.net>
2022-01-24 21:38:06 +0900

    ⚡️ add task list

--- src/components/onl-tasks.ts
+++ src/components/onl-tasks.ts
@@ -1,5 +1,17 @@
 import { LitElement, html, css } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, property } from 'lit/decorators.js';
+
+type SubTaskItem = {
+  id: string;
+  title: string;
+  due: string;
+}
+
+type TaskItem = {
+  id: string;
+  title: string;
+  items: SubTaskItem[];
+}
 
 @customElement('onl-tasks')
 export class OnlTasks extends LitElement {
@@ -9,8 +21,24 @@ export class OnlTasks extends LitElement {
     }
   `;
 
+  @property({ type: String }) serviceUrl!: string;
+
+  @property({ type: Object }) tasks!: TaskItem[];
+
   render() {
     return html`
+      ${this.tasks.map(({ title, items }) => (
+        html`
+          <h1>${title}</h1>
+          <ul>
+            ${items.map(({ title }) => (
+              html`
+                <li>${title}</li>
+              `
+            ))}
+          </ul>
+        `
+      ))}
       <p>Hello, world.</p>
     `;
   }
