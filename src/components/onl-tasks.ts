57cb484d6880d439acb1ca4daf67a88ce1b18075
lollipop-onl <lollipop@simochee.net>
2022-01-24 21:11:43 +0900

    ⚡️ Add esbuild

--- src/components/onl-tasks.ts
+++ src/components/onl-tasks.ts
@@ -1,5 +1,5 @@
 import { LitElement } from 'lit';
-import { customElement } from 'lit/decorators';
+import { customElement } from 'lit/decorators.js';
 
 @customElement('onl-tasks')
 export class OnlTasks extends LitElement {
